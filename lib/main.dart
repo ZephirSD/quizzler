b23e6cb6d2d6e405a405ae9d190c8478fdae050c
ZephirSD <siegfrieddallery@gmail.com>
2022-01-18 07:41:54 +0400

    Version 1 de Quizzler

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class Quizzler extends StatefulWidget {
 
 class _QuizzlerState extends State<Quizzler> {
   int questionNumber = 0;
+  int reponseNumber = 0;
   List<Icon> listIcons = [];
   List<String> questionList = [
     "Le piton des neiges est un volcan de la Réunion ?",
@@ -28,54 +29,77 @@ class _QuizzlerState extends State<Quizzler> {
   ];
   List<bool> reponseList = [true, true, false];
   buttonClick() {
+    int longueurQuestion = questionList.length;
     setState(() {
-      questionNumber++;
-      questionList[questionNumber];
+      if (questionNumber < longueurQuestion - 1) {
+        questionNumber++;
+        questionList[questionNumber];
+      }
+      if (reponseNumber < longueurQuestion) {
+        reponseNumber++;
+      }
     });
+    print('$questionNumber');
+    print('$reponseNumber');
   }
 
   trueButton() {
-    bool bonnereponse = reponseList[questionNumber];
     setState(() {
-      if (bonnereponse == true) {
-        listIcons.add(Icon(Icons.check, color: Colors.green));
-      } else {
-        listIcons.add(Icon(Icons.close, color: Colors.red));
+      int longueurResponse = reponseList.length;
+      bool bonnereponse = reponseList[questionNumber];
+      if (reponseNumber < longueurResponse) {
+        if (bonnereponse == true) {
+          listIcons.add(Icon(Icons.check, color: Colors.green));
+        } else {
+          listIcons.add(Icon(Icons.close, color: Colors.red));
+        }
       }
+      if (reponseNumber == longueurResponse - 1) {
+        alertScore(context);
+      }
+      print('$longueurResponse');
     });
     buttonClick();
   }
 
   falseButton() {
-    bool bonnereponse = reponseList[questionNumber];
     setState(() {
-      if (bonnereponse == false) {
-        listIcons.add(Icon(Icons.check, color: Colors.green));
-      } else {
-        listIcons.add(Icon(Icons.close, color: Colors.red));
+      int longueurResponse = reponseList.length;
+      bool bonnereponse = reponseList[questionNumber];
+      if (reponseNumber < longueurResponse) {
+        if (bonnereponse == false) {
+          listIcons.add(Icon(Icons.check, color: Colors.green));
+        } else {
+          listIcons.add(Icon(Icons.close, color: Colors.red));
+        }
       }
+      if (reponseNumber == longueurResponse - 1) {
+        alertScore(context);
+      }
+      print('$longueurResponse');
     });
     buttonClick();
   }
 
-  // falseButton() {
-  //   bool bonnereponse = reponseList[questionNumber];
-  //   int longueurQuestion = questionList.length;
-  //   print('$longueurQuestion');
-  //   if (longueurQuestion >= questionNumber) {
-  //     if (bonnereponse == false) {
-  //       setState(() {
-  //         listIcons.add(Icon(Icons.check, color: Colors.green));
-  //       });
-  //     } else {
-  //       setState(() {
-  //         listIcons.add(Icon(Icons.close, color: Colors.red));
-  //       });
-  //     }
-  //     print('$questionNumber');
-  //     print('$bonnereponse');
-  //   }
-  // }
+  void alertScore(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text("Vous avez terminé!!"),
+          content: new Text("Voici votre score"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
