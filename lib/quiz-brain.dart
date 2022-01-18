1a9357b953a255f808c44c2c4346389426503b29
ZephirSD <siegfrieddallery@gmail.com>
2022-01-18 13:50:54 +0400

    9 - Encapsulation

--- lib/quiz-brain.dart
+++ lib/quiz-brain.dart
@@ -1,9 +1,16 @@
 import 'question.dart';
 
 class QuizBrain {
-  List<Question> questions = [
+  List<Question> _questions = [
     Question("question1", true),
     Question("question2", false),
     Question("question3", true)
   ];
+  String getQuestionText(int questionNumber) {
+    return _questions[questionNumber].questionText;
+  }
+
+  bool getQuestionAnswer(int questionNumber) {
+    return _questions[questionNumber].questionAnswer;
+  }
 }
