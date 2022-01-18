6e3d25bb1b88c85c4282749c3c8c837d717eadeb
ZephirSD <siegfrieddallery@gmail.com>
2022-01-18 13:48:41 +0400

    8 - Abstraction

---
+++ lib/quiz-brain.dart
@@ -0,0 +1,9 @@
+import 'question.dart';
+
+class QuizBrain {
+  List<Question> questions = [
+    Question("question1", true),
+    Question("question2", false),
+    Question("question3", true)
+  ];
+}
